4a88a1606bde8dae9e558f9ffb68251064a84517
Chris Pebble <cvanopstal@gmail.com>
2011-11-22 08:13:31 -0600

    Added a Populate method to the Query object that takes a search string and uses it to initialize the object's properties.

--- GSALib/GSA/Query.cs
+++ GSALib/GSA/Query.cs
@@ -50,7 +50,8 @@ namespace GSALib.GSA
         public Access Access { get { return new Access(query.access); } set { query.access = value.getValue(); } }
         public string ProxyCustom { get { return query.proxycustom; } set { query.proxycustom = value; } }
         public string ProxyStylesheet { get { return query.proxystylesheet; } set { query.proxystylesheet = value; } }
-        public bool ProxyReload { get { return query.proxyreload; } set { query.proxyreload = value; } }   
+        public bool ProxyReload { get { return query.proxyreload; } set { query.proxyreload = value; } }
+        public string SiteSearch { get { return query.as_sitesearch; } set { query.as_sitesearch = value; } }
 
         public Query()
         {
@@ -102,12 +103,6 @@ namespace GSALib.GSA
         {
             return queryTerm.getValue();
         }
-
-        public void setAs_sitesearch(String as_sitesearch)
-        {
-            query.setAs_sitesearch(as_sitesearch);
-        }
-
         #endregion
     }
 
@@ -120,15 +115,14 @@ namespace GSALib.GSA
     public sealed class QueryTerm
     {
         #region Variables
-
-        private ArrayList inTitleTerms;
-        private ArrayList notInTitleTerms;
-        private ArrayList inUrlTerms;
-        private ArrayList notInUrlTerms;
-        private ArrayList includeFiletype;
-        private ArrayList excludeFiletype;
-        private ArrayList allInTitleTerms;
-        private ArrayList allInUrlTerms;
+        private ArrayList inTitleTerms = new ArrayList();
+        private ArrayList notInTitleTerms = new ArrayList();
+        private ArrayList inUrlTerms = new ArrayList();
+        private ArrayList notInUrlTerms = new ArrayList();
+        private ArrayList includeFiletype = new ArrayList();
+        private ArrayList excludeFiletype = new ArrayList();
+        private ArrayList allInTitleTerms = new ArrayList();
+        private ArrayList allInUrlTerms = new ArrayList();
         private String site;
         private bool includeSite;
         private String dateRange;
@@ -153,20 +147,99 @@ namespace GSALib.GSA
         private const String _LINK = "link:";
         private const String _OR = " OR ";
         private const String _SP = " ";
-
         #endregion
 
         #region Constructor
-
         public QueryTerm()
         {
 
         }
 
+        public QueryTerm(string queryString)
+        {
+            this.Populate(queryString);
+        }
+
+        /// <summary>
+        /// Extracts QueryTerm properties from a querystring.
+        /// </summary>
+        /// <param name="queryString"></param>
+        public void Populate(string queryString)
+        {
+            // pad the querystring with a space to make our job easier
+            queryString += _SP;
+
+            string[] flagArray = new string[] { _IN_TITLE, _NOT_IN_TITLE, _IN_URL, _NOT_IN_URL, _INCLUDE_FILETYPE, _EXCLUDE_FILETYPE, _INCLUDE_SITE, _EXCLUDE_SITE, _CACHE, _INFO, _LINK, _ALL_IN_TITLE, _ALL_IN_URL };
+            foreach (string flag in flagArray) {
+                // loop through all instances of the flag
+                while (queryString.Contains(flag)) {
+
+                    // extract the flag and value from our querystring for further processing
+                    int startIndex = queryString.IndexOf(flag);
+                    string extract = null;
+
+                    if (flag == _ALL_IN_URL || flag == _ALL_IN_TITLE) {
+                        // capture from flag till the next flag or the end of the line
+                        int nextFlagIndex = queryString.IndexOfAny(flagArray);
+                        extract = queryString.Substring(startIndex, nextFlagIndex != -1 ? nextFlagIndex : queryString.Length - startIndex);
+                    } else { // capture till first space
+                        extract = queryString.Substring(startIndex, queryString.IndexOf(_SP, startIndex));
+                    }
+
+                    // remove the extract from our querystring
+                    queryString.Remove(startIndex, extract.Length);
+
+                    // update the appropriate property
+                    string value = extract.Remove(0, flag.Length);
+                    switch (flag) {
+                        case _IN_TITLE:
+                            allInTitleTerms.Add(value);
+                            break;
+                        case _NOT_IN_TITLE:
+                            notInTitleTerms.Add(value);
+                            break;
+                        case _IN_URL:
+                            inUrlTerms.Add(value);
+                            break;
+                        case _NOT_IN_URL:
+                            notInTitleTerms.Add(value);
+                            break;
+                        case _INCLUDE_FILETYPE:
+                            includeFiletype.Add(value);
+                            break;
+                        case _EXCLUDE_FILETYPE:
+                            excludeFiletype.Add(value);
+                            break;
+                        case _INCLUDE_SITE:
+                            includeSite = true;
+                            site = value;
+                            break;
+                        case _EXCLUDE_SITE:
+                            includeSite = false;
+                            site = value;
+                            break;
+                        case _LINK:
+                            link = value;
+                            break;
+                        case _INFO:
+                            webDocLocation = value;
+                            break;
+                        case _CACHE:
+                            cacheDocLocation = value;
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                            break;
+                    }
+                }
+            }
+
+            // anything that is left over is the querystring
+            this.queryString = queryString;
+        }
         #endregion
 
         #region Get/Set Properties
-
         public void setQueryString(String queryString)
         {
             this.queryString = queryString;
@@ -269,73 +342,58 @@ namespace GSALib.GSA
             dateRange.Append(toDate.ToString("YYYY-MM-DD"));
             this.dateRange = dateRange.ToString();
         }
-
         #endregion
 
         #region getValue
-
         public String getValue()
         {
             String retval = null;
             StringBuilder qbuf = new StringBuilder();
-            if (allInTitleTerms != null && allInTitleTerms.Count  > 0)
-            {
+            if (allInTitleTerms != null && allInTitleTerms.Count > 0) {
                 qbuf.Append(_ALL_IN_TITLE).Append(Util.SeparatedString(allInTitleTerms, null, _SP)).Append(' ');
             }
-            if (allInUrlTerms != null && allInUrlTerms.Count > 0)
-            {
+            if (allInUrlTerms != null && allInUrlTerms.Count > 0) {
                 qbuf.Append(_ALL_IN_URL).Append(Util.SeparatedString(allInUrlTerms, null, _SP)).Append(' ');
             }
 
-            if (webDocLocation != null && webDocLocation.Length  > 0)
-            {
+            if (webDocLocation != null && webDocLocation.Length > 0) {
                 qbuf.Append(_INFO).Append(webDocLocation).Append(' ');
             }
 
-            if (cacheDocLocation != null && cacheDocLocation.Length > 0)
-            {
+            if (cacheDocLocation != null && cacheDocLocation.Length > 0) {
                 qbuf.Append(_CACHE).Append(cacheDocLocation).Append(' ');
             }
-            if (link != null && link.Length > 0)
-            {
+            if (link != null && link.Length > 0) {
                 qbuf.Append(_LINK).Append(link);
             }
 
-            if (inTitleTerms != null && inTitleTerms.Count > 0)
-            {
+            if (inTitleTerms != null && inTitleTerms.Count > 0) {
                 qbuf.Append(Util.SeparatedString(inTitleTerms, _IN_TITLE, _SP)).Append(' ');
             }
-            if (notInTitleTerms != null && notInTitleTerms.Count > 0)
-            {
+            if (notInTitleTerms != null && notInTitleTerms.Count > 0) {
                 qbuf.Append(Util.SeparatedString(notInTitleTerms, _NOT_IN_TITLE, _SP)).Append(' ');
             }
 
-            if (inUrlTerms != null && inUrlTerms.Count > 0)
-            {
+            if (inUrlTerms != null && inUrlTerms.Count > 0) {
                 qbuf.Append(Util.SeparatedString(inUrlTerms, _IN_URL, _SP)).Append(' ');
             }
-            if (notInUrlTerms != null && notInUrlTerms.Count > 0)
-            {
+            if (notInUrlTerms != null && notInUrlTerms.Count > 0) {
                 qbuf.Append(Util.SeparatedString(notInUrlTerms, _NOT_IN_URL, _SP)).Append(' ');
             }
 
-            if (site != null && site.Length > 0)
-            {
+            if (site != null && site.Length > 0) {
                 qbuf.Append(includeSite
                         ? _INCLUDE_SITE
                         : _EXCLUDE_SITE).Append(site).Append(' ');
             }
 
-            if (includeFiletype != null && includeFiletype.Count > 0)
-            {
+            if (includeFiletype != null && includeFiletype.Count > 0) {
                 qbuf.Append(Util.SeparatedString(includeFiletype, _INCLUDE_FILETYPE, _OR)).Append(' ');
             }
-            if (excludeFiletype != null && excludeFiletype.Count > 0)
-            {
+            if (excludeFiletype != null && excludeFiletype.Count > 0) {
                 qbuf.Append(Util.SeparatedString(excludeFiletype, _EXCLUDE_FILETYPE, _SP)).Append(' ');
             }
-            if (dateRange != null)
-            {
+            if (dateRange != null) {
                 qbuf.Append(_DATERANGE).Append(dateRange).Append(' ');
             }
 
@@ -344,7 +402,6 @@ namespace GSALib.GSA
             retval = qbuf.ToString();
             return retval;
         }
-
         #endregion       
     }
 }
